8c9cfcb69d3fad6d3fe8d1985e4fd0e9eff708b1
MelPichery <melanie.pichery@laposte.net>
2022-11-04 10:37:11 +0100

    ProjectEuler1

---
+++ src/SumOf3and5Multiples.java
@@ -0,0 +1,17 @@
+public class SumOf3and5Multiples {
+
+    public int returnSumOf3and5Multiples(int numberChoosen) {
+        int number = 0;
+        int sum = 0;
+        while(number<numberChoosen) {
+
+            if(number%3 == 0 || number%5 ==0){
+                sum = sum+number;
+            }
+
+            number++;
+        }
+        return sum;
+    }
+
+}
